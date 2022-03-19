9604c117e5a71eda29d52e77b4970e94138f75e1
Etizaz Ahsan <etizaz7ahsan@gmail.com>
2022-03-20 00:53:50 +0500

    backend url updated

--- src/server.ts
+++ src/server.ts
@@ -5,7 +5,7 @@ const { REACT_APP_API_URL } = process.env
 console.log({ REACT_APP_API_URL })
 
 export const server = axios.create({
-  baseURL: 'https://pixsy-task.herokuapp.com//api/v1/pixsy',
+  baseURL: 'https://pixsy-task.herokuapp.com/api/v1/pixsy',
   timeout: 1000,
   // headers: { 'X-Custom-Header': 'foobar' }
 });
